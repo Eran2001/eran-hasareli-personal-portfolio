3c806b5c11a6bd4b28115a8c3984d556d66e147a
Eran Hasareli <126740413+Eran2001@users.noreply.github.com>
2026-03-20 08:43:42 +0530

    Remove commented vite.config.js reference

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
   test: {
